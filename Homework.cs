f8b432005d5174cd259513f23716e96a82761e30
ArikPoghosyan <poxosyanarik07@gmail.com>
2023-10-06 19:53:41 +0400

    C#_Mendeleev_1

---
+++ Homework.cs
@@ -0,0 +1,20 @@
+﻿List<string> input = new List<string>{"sd","kio","dio"};
+List<string> output = new List<string>{};
+while (true){
+    for(int i = 0;i< a.length;i++){
+        int f =0;
+        char lastl = i.length;
+        char first = i.^length;
+        if(a[f][lastl] != a[i][lastl]){
+            continue;
+        }
+        else{
+            out.Add(a[f])
+        }
+        f++;
+        if(f>=a.length){
+            System.Console.WriteLine();
+            break;
+        }
+    }
+}
